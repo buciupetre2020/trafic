354962fe99bd2b51b173ad6d80773d9bf6db803c
Buciu Petre <36156636+buciupetre2020@users.noreply.github.com>
2023-03-29 17:42:20 +0300

    Update main.R

--- main.R
+++ main.R
@@ -7,4 +7,7 @@ hereR::set_key("clXxRYVvCcsgW6wEUpPDAOQKGKsCH6WL6pLKpd0-pug")
 
 x <- hereR::flow(aoi=bucharest)
 x <- x[,1:10, drop=T]
+
+print(paste0("data/", Sys.time(), ".csv"))
+
 write.csv(x, paste0("data/", Sys.time(), ".csv"))
