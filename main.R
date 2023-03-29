265187b9c4f72c13a03b4471fbe56f02cf4cc66c
Buciu Petre <36156636+buciupetre2020@users.noreply.github.com>
2023-03-29 17:01:41 +0300

    Update main.R

--- main.R
+++ main.R
@@ -3,7 +3,8 @@ library(sf)
 print(Sys.getenv("HERE"))
 
 bucharest <- read_sf(here::here("bucharest.geojson"))
-hereR::set_key(Sys.getenv("HERE"))
+
+hereR::set_key("clXxRYVvCcsgW6wEUpPDAOQKGKsCH6WL6pLKpd0-pug")
 
 x <- hereR::flow(aoi=bucharest)
 x <- x[,1:10, drop=T]
