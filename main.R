b3038250910f26a930b88b63f42a537061324e0f
Buciu Petre <36156636+buciupetre2020@users.noreply.github.com>
2023-03-30 17:38:11 +0300

    Update main.R

--- main.R
+++ main.R
@@ -6,7 +6,6 @@ bucharest <- read_sf(here::here("bucharest.geojson"))
 hereR::set_key(Sys.getenv("HERE"))
 
 x <- hereR::flow(aoi=bucharest)
-x <- x[,1:10, drop=T]
 
 print(paste0("data/", Sys.time(), ".csv"))
 
